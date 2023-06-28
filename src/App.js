fc63d4b7860c6c109e7701cccccf36015e897c17
nanr1901 <nandana.rajancr@gmail.com>
2023-06-28 19:07:46 +0530

    remaining navbar pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import SignUp from './routes/SignUp/SignUp';
 import StudentSignIn from './routes/SignIn/StudentSignIn/StudentSignIn';
 import RecSignIn from './routes/SignIn/StudentSignIn/RecSignIn';
 import RecruiterProfile from './routes/RecruiterProfile/RecruiterProfile';
+import ContactUs from './routes/ContactUs/ContactUs';
+import Academics from './routes/Academics/Academics';
+import PastRecruiters from './routes/PastRecruiters/PastRecruiters';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -18,7 +21,9 @@ function App() {
           <Route path="/StudentSignIn" element={<StudentSignIn/>}/>
           <Route path="/RecSignIn" element={<RecSignIn/>}/>
           <Route path="/RecruiterProfile" element={<RecruiterProfile/>}/>
-
+          <Route path="/ContactUs" element={<ContactUs/>}/>
+          <Route path="/Academics" element={<Academics/>}/>
+          <Route path="/PastRecruiters" element={<PastRecruiters/>}/>
           {/* <Route path="/mim" element={<MimicScreen />} /> */}
           
         </Routes>
--- src/routes/NavBar/NavBar.js
+++ src/routes/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavBar.css";
+import { Link } from 'react-router-dom';
 import logo from "./Components/NITT Logo - White 1.png"
 
 const NavBar=()=>{
@@ -12,10 +13,12 @@ return(
   <div className="title-text">NITT Placement Portal</div>
   </div>
    <ul>
-  <li>Home</li>
-  <li>Academics</li>
-  <li>Past recruiters</li>
-  <li>Contact us</li>
+  <li>
+  <Link to="/">Home </Link>
+  </li>
+  <li> <Link to="/Academics">Academics </Link></li>
+  <li> <Link to="/PastRecruiters"> Past recruiters  </Link></li>
+  <li> <Link to="/ContactUs">Contact us </Link></li>
 </ul>
     </div>
   
